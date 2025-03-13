5c38c9e18de158c2b7cbd7f4014410b0b01f1e37
senator423 <oumasenator@gmail.com>
2025-03-14 02:24:09 +0300

    first commit

--- my_petapp/admin.py
+++ my_petapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import BlogPost, Booking, Contact
 
-# Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    search_fields = ('title', 'author')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'service', 'date', 'time')
+    search_fields = ('name', 'email', 'service')
+    list_filter = ('date', 'service')
+    ordering = ('-date',)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'submitted_at')
+    search_fields = ('name', 'email')
+    ordering = ('-submitted_at',)
+
