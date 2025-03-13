db355c0a1fce4f099ec926781d3b4495511ab25d
senator423 <oumasenator@gmail.com>
2025-03-13 23:57:48 +0300

    first commit

--- pet_buisness/settings.py
+++ pet_buisness/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'pet_buisness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pet-business',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
